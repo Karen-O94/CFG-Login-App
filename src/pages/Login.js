3bc6a9c9a470ee27dc51dd2fac84c1fb8a8d3c68
Karen <karenyaa@live.co.uk>
2023-05-16 21:48:03 +0100

    moved Login, LoginSuccess and Error components into a pages folder

---
+++ src/pages/Login.js
@@ -0,0 +1,29 @@
+function Login(){
+    return (
+<div class="row justify-content-center">
+  <div class="col-md-6 p-3">
+    <h2 class="titles">LOGIN</h2>
+
+    <form class="form login-form">
+      <div class="form-group">
+        <label for="email-login">Username: </label>
+        <br />
+        <input class="form-input" type="text" id="email-login" />
+      </div>
+
+      <div class="form-group mb-3">
+        <label for="password-login">Password: </label>
+        <br />
+        <input class="form-input" type="password" id="password-login" />
+      </div>
+
+      <div class="form-group mb-3">
+        <button class="btn btn-lg login-btn" type="submit">Login</button>
+      </div>
+    </form>
+  </div>
+</div>
+    )
+}
+
+export default Login;
