ba6c0f9e44cc4e96167acdaf88c1c129bced455a
Karen <karenyaa@live.co.uk>
2023-05-16 22:01:41 +0100

    amended loginSuccess page name

---
+++ src/pages/LoginSuccess.js
@@ -0,0 +1,21 @@
+function LoginSuccess() {
+  return (
+    <div class="row justify-content-center">
+      <div class="col-md-6 p-3">
+        <h2 class="titles">Login Success!</h2>
+        <div>
+          <h1>Welcome! You have successfully logged in!</h1>
+        </div>
+
+        <div class="form-group mb-3">
+          Click here to sign out
+          <button class="btn btn-lg login-btn" type="submit">
+            LogOut
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginSuccess;
