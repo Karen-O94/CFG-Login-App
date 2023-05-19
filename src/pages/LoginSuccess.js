9e10c2ba3ffb92f4ffb474cec2c5ebf886b76c30
Karen <karenyaa@live.co.uk>
2023-05-19 08:35:14 +0100

    added useState to check for whether user is logged in and to redirect users back to login if not validated

--- src/pages/LoginSuccess.js
+++ src/pages/LoginSuccess.js
@@ -1,22 +1,39 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-function LoginSuccess() {
-  return (
-    <div className="row justify-content-center">
-      <div className="col-md-6 p-3">
-        <h2 className="titles">Login Success!</h2>
-        <div>
-          <h1>Welcome! You have successfully logged in!</h1>
-        </div>
+import { useEffect, useState } from 'react';
 
-        <div className="form-group mb-3">
-          <h5>Click here to sign out</h5>
-          <button className="btn btn-lg btn-danger logout-btn" type="submit">
-            LogOut
-          </button>
+//LoginSuccess route to be displayed when user is logged in
+function LoginSuccess() {
+  const [validated, setValidation] = useState(null)
+  useEffect (() => {
+    const userIsLoggedIn = localStorage.getItem("validated");
+    if (userIsLoggedIn) {
+      setValidated(userIsLoggedIn)
+    }
+  }, []);
+
+  //If user is not logged in, they will need to be redirected to Login (Home page)
+  if (!validated) {
+    
+
+  } else {
+    return (
+      <div className="row justify-content-center">
+        <div className="col-md-6 p-3">
+          <h2 className="titles">Login Success!</h2>
+          <div>
+            <h1>Welcome! You have successfully logged in!</h1>
+          </div>
+  
+          <div className="form-group mb-3">
+            <h5>Click here to sign out</h5>
+            <button className="btn btn-lg btn-danger logout-btn" type="submit">
+              LogOut
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+};
 
 export default LoginSuccess;
